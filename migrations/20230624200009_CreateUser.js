32105dbd0142a1e9d0b99cb8395bf9cab7a112b0
Siddeshwarbijji <116542236+Siddeshwarbijji@users.noreply.github.com>
2023-06-24 13:19:18 +0530

    Update 20230624200009_CreateUser.js

--- migrations/20230624200009_CreateUser.js
+++ migrations/20230624200009_CreateUser.js
@@ -24,20 +24,4 @@ module.exports = {
       },
       isAdmin: {
         type: Sequelize.BOOLEAN,
-        defaultValue: false
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
-  }
-};
+        defaultValue
